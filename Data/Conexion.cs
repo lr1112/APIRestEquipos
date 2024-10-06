46f02bf23f4b50640cf2b7ac41a53968805a37eb
lr1112 <luis_rosario_0324@hotmail.com>
2024-10-05 23:36:44 -0400

    creando la conexion con la base de datos

--- Data/Conexion.cs
+++ Data/Conexion.cs
@@ -2,6 +2,7 @@
 {
     public class Conexion
     {
+        public static string rutaConexion = "Data Source=DESKTOP-5IQMLRU\\SQLEXPRESS;Initial Catalog=EquiposDb;Integrated Security=True;Trust Server Certificate=True";
 
     }
 }
