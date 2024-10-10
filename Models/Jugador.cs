c80f675a8819fa7ceb7ae56d92389cfe9d46bc5a
lr1112 <luis_rosario_0324@hotmail.com>
2024-10-10 17:04:05 -0400

    modificando la clase jugador

--- Models/Jugador.cs
+++ Models/Jugador.cs
@@ -5,11 +5,14 @@
         public int id { get; set; }
         public string nombre { get; set; }
         public string apellido { get; set; }
+        public int lanzamientos { get; set; }
         public int puntos { get; set; }
         public int asistencias { get; set; }
         public int rebotes { get; set; }
         public int robos { get; set; }
         public int perdidas { get; set; }
+        public int triples { get; set; }
+
 
     }
 }
