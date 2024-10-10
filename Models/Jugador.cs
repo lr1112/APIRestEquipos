dc84648fc2bcb96fdc97def1ed288770fd47b339
lr1112 <luis_rosario_0324@hotmail.com>
2024-10-10 16:34:55 -0400

    creando la clase Jugador

---
+++ Models/Jugador.cs
@@ -0,0 +1,15 @@
+﻿namespace APIRestEquipos.Models
+{
+    public class Jugador
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public int puntos { get; set; }
+        public int asistencias { get; set; }
+        public int rebotes { get; set; }
+        public int robos { get; set; }
+        public int perdidas { get; set; }
+
+    }
+}
